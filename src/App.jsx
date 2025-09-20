0ac4643e1e2be6e18f8bafcd9b475d441d9d1e40
kbamnote3 <kunalbamnote7@gmail.com>
2025-09-20 11:03:13 +0530

    sixth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import LandingPage from './components/pages/landingPage/LandingPage'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <LandingPage/>
+     <SpeedInsights/>
     </>
   )
 }
