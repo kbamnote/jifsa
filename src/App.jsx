490f95b38ae2584018a3c1e95a7a1a82cbfedf5e
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-09-23 17:55:34 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import MembershipAndAcc from "./components/pages/aboutUsPage/dropdown/membership
 import CoursePage from "./components/pages/coursesPage/qualificationPage/CoursePage";
 import OnlineCoursePage from "./components/pages/coursesPage/onlineCoursePage/OnlineCoursePage";
 import FaqPage from "./components/pages/coursesPage/faqPage/FaqPage";
+import AdvisoryBoard from "./components/pages/aboutUsPage/dropdown/advisory-board/AdvisoryBoard";
+import ChairmanMessage from "./components/pages/aboutUsPage/dropdown/chairman-message/ChairmanMessage";
+import VchairpersonMessage from "./components/pages/aboutUsPage/dropdown/vice-chairperson-message/VchairpersonMessage";
+import DirectorsMessage from "./components/pages/aboutUsPage/dropdown/director-message/DirectorsMessage";
 
 function App() {
   return (
@@ -20,6 +24,10 @@ function App() {
         <Route path="/resedent" element={<ResedentPage />} />
         <Route path="/vision-mission" element={<JifsaVandM />} />
         <Route path="/membership" element={<MembershipAndAcc />} />
+        <Route path="/advisory-board" element={<AdvisoryBoard/>}/>
+        <Route path="/chairman-message" element={<ChairmanMessage/>}/>
+        <Route path="/vice-chairperson-message" element={<VchairpersonMessage/>}/>
+        <Route path="/directors-message" element={<DirectorsMessage/>}/>
         <Route path="/qualifications" element={<CoursePage />} />
         <Route path="/online-courses" element={<OnlineCoursePage />} />
         <Route path="/fire-safety-faqs" element={<FaqPage />} />
--- src/components/pages/aboutUsPage/dropdown/advisory-board/AdvisoryBoard.jsx
+++ src/components/pages/aboutUsPage/dropdown/advisory-board/AdvisoryBoard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Header from "../../../../common/Header";
+import Navbar from "../../../../common/Navbar";
+import Footer from "../../../../common/Footer";
+import banner from "../../../../../assets/ban2.jpg";
+import { Link } from "react-router-dom";
+import AbContents from "./abContents/AbContents";
 
 const AdvisoryBoard = () => {
   return (
-    <div>AdvisoryBoard</div>
-  )
-}
+    <>
+      <Header />
+      <Navbar />
+      <div
+        className="relative min-h-[200px] sm:min-h-[250px] md:min-h-[350px] lg:min-h-[350px] xl:min-h-[360px] flex items-center text-white bg-center bg-no-repeat bg-contain"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        {/* Gradient Overlay */}
+        <div className="absolute inset-0 bg-gradient-to-r from-[#1e1e2f]/90 to-[#0a0a0f]/20"></div>
 
-export default AdvisoryBoard
+        {/* Content */}
+        <div className="relative z-10 text-left px-4 sm:px-8 md:px-[5%]">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-2 sm:mb-3">
+            Advisory Committee
+          </h1>
+          <p className="text-xs sm:text-sm">
+            <Link to="/" className="hover:underline">
+              HOME
+            </Link>{" "}
+            <span className="mx-2">&gt;</span> Advisory Committee
+          </p>
+        </div>
+      </div>
+      <div className="w-full pt-3 bg-gray-100">
+        <AbContents/>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AdvisoryBoard;
