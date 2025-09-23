681b2498848d163a3afb6a61d91dc27c9c2b9311
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-09-23 14:56:28 +0530

    hdt

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import HeroGallery from "./components/pages/galleryPage/HeroGallery";
 import ResedentPage from "./components/pages/resedentialPage/ResedentPage";
 import JifsaVandM from "./components/pages/aboutUsPage/dropdown/jifsaVandM/JifsaVandM";
 import MembershipAndAcc from "./components/pages/aboutUsPage/dropdown/membership-and-acc/MembershipAndAcc";
-import CoursePage from './components/pages/coursesPage/qualificationPage/CoursePage';
-import OnlineCoursePage from './components/pages/coursesPage/onlineCoursePage/OnlineCoursePage';
-import FaqPage from './components/pages/coursesPage/faqPage/FaqPage';
-
+import CoursePage from "./components/pages/coursesPage/qualificationPage/CoursePage";
+import OnlineCoursePage from "./components/pages/coursesPage/onlineCoursePage/OnlineCoursePage";
+import FaqPage from "./components/pages/coursesPage/faqPage/FaqPage";
 
 function App() {
   return (
@@ -21,9 +20,9 @@ function App() {
         <Route path="/resedent" element={<ResedentPage />} />
         <Route path="/vision-mission" element={<JifsaVandM />} />
         <Route path="/membership" element={<MembershipAndAcc />} />
-        <Route path="/qualifications" element={<CoursePage/>} />
-                <Route path="/online-courses" element={<OnlineCoursePage/>} />
-                   <Route path="/fire-safety-faqs" element={<FaqPage/>} />
+        <Route path="/qualifications" element={<CoursePage />} />
+        <Route path="/online-courses" element={<OnlineCoursePage />} />
+        <Route path="/fire-safety-faqs" element={<FaqPage />} />
       </Routes>
 
       <SpeedInsights />
--- src/components/pages/aboutUsPage/dropdown/membership-and-acc/MembershipAndAcc.jsx
+++ src/components/pages/aboutUsPage/dropdown/membership-and-acc/MembershipAndAcc.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../../../../common/Header'
+import Navbar from '../../../../common/Navbar'
+import Footer from '../../../../common/Footer'
+import logo1 from '../../../../../assets/lo1.png'
+import logo2 from '../../../../../assets/lo2.png'
+import logo3 from '../../../../../assets/lo3.png'
+import logo4 from '../../../../../assets/lo4.png'
+import logo5 from '../../../../../assets/lo5.png'
 
 const MembershipAndAcc = () => {
   return (
-    <div>MembershipAndAcc</div>
+    <>
+    <Header/>
+    <Navbar/>
+    <div>
+
+    </div>
+    <Footer/>
+    </>
   )
 }
 
