4558c8cd6c4b5cb8813f8f886ecfbe8ebcc98e49
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-09-23 14:18:20 +0530

    h2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import HeroGallery from "./components/pages/galleryPage/HeroGallery";
 import ResedentPage from "./components/pages/resedentialPage/ResedentPage";
 import JifsaVandM from "./components/pages/aboutUsPage/dropdown/jifsaVandM/JifsaVandM";
 import MembershipAndAcc from "./components/pages/aboutUsPage/dropdown/membership-and-acc/MembershipAndAcc";
+import CoursePage from './components/pages/coursesPage/qualificationPage/CoursePage';
+import OnlineCoursePage from './components/pages/coursesPage/onlineCoursePage/OnlineCoursePage';
+import FaqPage from './components/pages/coursesPage/faqPage/FaqPage';
+
 
 function App() {
   return (
@@ -17,6 +21,9 @@ function App() {
         <Route path="/resedent" element={<ResedentPage />} />
         <Route path="/vision-mission" element={<JifsaVandM/>}/>
         <Route path="/membership" element={<MembershipAndAcc/>}/>
+        <Route path="/qualifications" element={<CoursePage/>} />
+                <Route path="/online-courses" element={<OnlineCoursePage/>} />
+                   <Route path="/fire-safety-faqs" element={<FaqPage/>} />
       </Routes>
 
       <SpeedInsights />
---
+++ src/components/pages/aboutUsPage/dropdown/chairman-message/ChairmanMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChairmanMessage = () => {
+  return (
+    <div>ChairmanMessage</div>
+  )
+}
+
+export default ChairmanMessage
